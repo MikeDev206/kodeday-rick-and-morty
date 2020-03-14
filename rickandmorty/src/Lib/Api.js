5953115b15f9a7c0037efe0c2769a5c2a095c15c
Miguel Angel Aceves <acevesm77@gmail.com>
2020-03-14 13:15:34 -0600

    random added and app finished

--- rickandmorty/src/Lib/Api.js
+++ rickandmorty/src/Lib/Api.js
@@ -1,11 +1,13 @@
 
 async function getAllCharacters() {
-  const response = await fetch('https://rickandmortyapi.com/api/character/')
+  const randomPage = Math.round(Math.random()*25)
+  const response = await fetch(`https://rickandmortyapi.com/api/character/?page=${randomPage}`)
   const parsedJson = await response.json()
   return parsedJson.results
 
 
 }
+
 
 async function getCharacterById(id){
   const response = await fetch(`https://rickandmortyapi.com/api/character/${id}`)
