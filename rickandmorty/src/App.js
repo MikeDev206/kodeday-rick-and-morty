632bc56c4023f19f3ea18d0e65ae6173e282d515
Miguel Angel Aceves <acevesm77@gmail.com>
2020-03-14 09:23:15 -0600

    modified to create webapp of rick & morty

--- rickandmorty/src/App.js
+++ rickandmorty/src/App.js
@@ -2,25 +2,43 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  renderCards() {
+    return( 
+      <div className="card">
+        <div className="card-img">
+          <figure>
+            <img src={"https://rickandmortyapi.com/api/character/avatar/427.jpeg"}  />
+          </figure>
+        </div>
+        <div className="card-description">
+          <div className="card-name">
+            <h3> RICK </h3>
+          </div>
+        </div>
+      </div>
+            
+    )
+  }
 
+  render() {
+
+    const fakeData = [0,1,2,3,4,5,6,7]
+    const cards = fakeData.map(prro => this.renderCards())
+    console.log(cards)
+    return (
+      <div className="app">
+        <div className="app-container">
+          <h1> Ricardo y Martin</h1>
+          <div className="card-container">
+            {cards}
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+  }
+
+  
 export default App;
